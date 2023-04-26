6bbd3910120cfad625d9bbf785dce00cb675404d
muhammetali35 <129202058+muhammetali35@users.noreply.github.com>
2023-04-26 22:01:34 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,6 +2,11 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir.");
         //versiyon1
+        System.out.println("Java is easy");
+        //version2
+        System.out.println("Learn Java earn money");
+        //versiyon3
+
     }
 
 
